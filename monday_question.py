b98dbc0b2c3545f7861ff5aba99891d965a2ecdf
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-07-14 10:39:33 -0700

    Added queues

--- monday_question.py
+++ monday_question.py
@@ -5,3 +5,15 @@ for x in y:
 
 # Can also be ran as shown below!
 # print(*y, sep = "\n")
+
+z = ['Bob', 'Slack', ['reddit', '89', 101, ['alacritty', '(brackets)', 5, 375]], 0, ['{slice, owned}'], 22]
+
+
+def print_list(lst):
+    for i in lst:
+        if type(i) is not type([]):
+            print(i)
+        else:
+            print_list(i)
+
+print_list(z)
