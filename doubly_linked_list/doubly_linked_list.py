8b4530e7572e660362b5a6e371429e7382828800
Travis Cain <Travis@Traviss-MacBook-Air.local>
2020-07-14 13:35:47 -0700

    Finished Module 2, more work to do

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -7,6 +7,24 @@ class ListNode:
         self.prev = prev
         self.value = value
         self.next = next
+
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, self, current_prev)
+        if current_prev:
+            current_prev.next = self.prev
+    
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
             
 """
 Our doubly-linked list class. It holds references to 
@@ -27,7 +45,14 @@ class DoublyLinkedList:
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if self.head:
+            new_node.next = self.head
+            self.head.prev = new_node
+        else:
+            self.tail = new_node
+        self.head = new_node
         
     """
     Removes the List's current head node, making the
@@ -35,7 +60,9 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
+        val = self.head.value
+        self.delete(self.head)
+        return val
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -43,7 +70,14 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value)
+        self.length += 1
+        if self.tail:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+        else:
+            self.head = new_node
+        self.tail = new_node
             
     """
     Removes the List's current tail node, making the 
@@ -51,32 +85,69 @@ class DoublyLinkedList:
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
+        val = self.tail.value
+        self.delete(self.tail)
+        return val
             
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        val = node.value
+        self.delete(node)
+        self.add_to_head(val)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        val = node.value
+        self.delete(node)
+        self.add_to_tail(val)
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        # for an empty list
+        if not self.head:
+            print('Nothing to see here')
+            return
+        
+        self.length -= 1
+
+        # only one item in list
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+
+        # 2 items and desired deletion is the head
+        if node == self.head:
+            self.head = node.next
+            self.head.prev = None
+
+        # 2 items and desired deletion is the tail
+        if node == self.tail:
+            self.tail = node.next
+            self.head.prev = None
+
+        else:
+                node.delete()
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
     def get_max(self):
-        pass
+        val = self.head.value
+        example = self.head
+
+        while example is not None:
+            if example.value > val:
+                val = example.value
+            
+            example = example.next
+        return val
